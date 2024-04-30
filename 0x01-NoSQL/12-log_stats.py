88b8d1e7172f00c90ec8837209ca560599806b45
aezzahir <aero.glowsky@gmail.com>
2024-04-30 17:57:56 +0100

    alx backend storage

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+""" MongoDB Operations with Python"""
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ Provides statistics about Nginx logs stored in MongoDB """
-    # Establish connection to MongoDB
+    """ Provides some stats"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    
-    # Access the nginx collection
     nginx_collection = client.logs.nginx
 
-    # Count the total number of logs
-    total_logs = nginx_collection.count_documents({})
-    print(f'Total number of logs: {total_logs}')
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
 
-    # Count the number of logs for each HTTP method
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f'\t{method}: {count}')
+        print(f'\tmethod {method}: {count}')
 
-    # Count the number of status check logs
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print(f'Number of status check logs: {status_check}')
+
+    print(f'{status_check} status check')
