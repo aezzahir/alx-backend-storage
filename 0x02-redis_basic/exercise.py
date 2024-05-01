83711c2ea4c52a9679317af92659a7dba529eb38
aezzahir <aero.glowsky@gmail.com>
2024-05-01 12:12:43 +0100

    0x02-redis_basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,14 +5,12 @@ This Module handles cache ....
 import redis
 import uuid
 
+
 class Cache(object):
-
-
 
     def __init__(self):
         self._redis = redis.Redis()
-        self._redis.flushdb() # Flush database: clear old entries
-    
+        self._redis.flushdb()
 
     def store(self, data) -> str:
         key = str(uuid.uuid4())
